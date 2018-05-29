cf0d9bf7b6f16753aa24916e7782905339c589a7
Mohamed <mohamedr201@gmail.com>
2018-05-29 23:13:38 +0000

    Added SCSS variables

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -98,7 +98,7 @@ console.log(a * 15);
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+throw new Error("Module build failed: ModuleBuildError: Module build failed: \n$success-font-color:color('green')\n^\n      Invalid CSS after \"...cess-font-color\": expected 1 selector or at-rule, was \":color('green')\"\n      in /home/ubuntu/workspace/src/scss/base/_variables.scss (line 56, column 1)\n    at runLoaders (/home/ubuntu/workspace/node_modules/webpack/lib/NormalModule.js:252:20)\n    at /home/ubuntu/workspace/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /home/ubuntu/workspace/node_modules/loader-runner/lib/LoaderRunner.js:230:18\n    at context.callback (/home/ubuntu/workspace/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (/home/ubuntu/workspace/node_modules/sass-loader/lib/loader.js:76:13)\n    at Object.done [as callback] (/home/ubuntu/workspace/node_modules/neo-async/async.js:7974:18)\n    at options.error (/home/ubuntu/workspace/node_modules/node-sass/lib/index.js:294:32)");
 
 /***/ })
 
